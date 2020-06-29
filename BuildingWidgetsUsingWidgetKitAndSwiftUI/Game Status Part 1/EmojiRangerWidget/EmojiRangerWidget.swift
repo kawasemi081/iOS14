547ab32fb1cd27d06eb62934fb52b2ae896f33fe
midori2012 <misono5861@gmail.com>
2020-06-29 23:10:54 +0900

    add demo part2

--- BuildingWidgetsUsingWidgetKitAndSwiftUI/Game Status Part 1/EmojiRangerWidget/EmojiRangerWidget.swift
+++ BuildingWidgetsUsingWidgetKitAndSwiftUI/Game Status Part 1/EmojiRangerWidget/EmojiRangerWidget.swift
@@ -8,32 +8,57 @@
 
 import WidgetKit
 import SwiftUI
-import Intents
 
-struct Provider: TimelineProvider {
-    public typealias Entry = SimpleEntry
-    
-    public func snapshot(with context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), charactor: .panda)
-        completion(entry)
-    }
+struct Provider: IntentTimelineProvider {
+    func timeline(for configuration: CharacterSelectionIntent, with context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
+        let selectedCharactor = caracter(for: configuration)
+        let endDate = selectedCharactor.fullHealthDate
+        let oneMinute: TimeInterval = 60
+        var currentDate = Date()
 
-    public func timeline(with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        let entries: [SimpleEntry] = [SimpleEntry(date: Date(), charactor: .panda)]
+        var entries: [SimpleEntry] = []
+        while currentDate < endDate {
+            let relevance = TimelineEntryRelevance(score: Float(selectedCharactor.healthLevel))
+            let entry = SimpleEntry(date: currentDate, charactor: selectedCharactor, relevance: relevance)
+            currentDate += oneMinute
+            entries.append(entry)
+        }
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
+    }
+    
+    typealias Intent = CharacterSelectionIntent
+    public typealias Entry = SimpleEntry
+
+    func caracter(for configuration: CharacterSelectionIntent) -> CharacterDetail {
+        switch configuration.hero {
+        case .panda:
+            return .panda
+        case .egghead:
+            return .egghead
+        case .spouty:
+            return .spouty
+        default:
+            return .panda
+        }
+    }
+
+    public func snapshot(for configuration: CharacterSelectionIntent, with context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), charactor: .panda, relevance: nil)
+        completion(entry)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     public let date: Date
     public let charactor: CharacterDetail
+    let relevance: TimelineEntryRelevance?
 }
 
 struct PlaceholderView : View {
     var body: some View {
-        AvatarView(.panda)
+        EmojiRangerWidgetEntryView(entry: SimpleEntry(date: Date(), charactor: .panda, relevance: nil))
         /// - Attention: This is not available yet.
         /// - seeAlso: https://developer.apple.com/forums/thrzead/650564
 //            .isPlaceholder(true)
@@ -42,9 +67,30 @@ struct PlaceholderView : View {
 
 struct EmojiRangerWidgetEntryView : View {
     var entry: Provider.Entry
+    @Environment(\.widgetFamily) var family
 
+    @ViewBuilder
     var body: some View {
-        AvatarView(entry.charactor)
+        switch family {
+        case .systemSmall:
+            ZStack {
+                AvatarView(entry.charactor)
+                    .foregroundColor(.white)
+            }.background(Color.gameBackground)
+        default:
+            ZStack {
+                HStack {
+                    AvatarView(entry.charactor)
+                    Text(entry.charactor.bio)
+                        .padding()
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .widgetURL(entry.charactor.url)
+            }
+            .background(Color.gameBackground)
+        }
+        
     }
 }
 
@@ -53,12 +99,12 @@ struct EmojiRangerWidget: Widget {
     private let kind: String = "EmojiRangerWidget"
 
     public var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider(), placeholder: PlaceholderView()) { entry in
+        IntentConfiguration(kind: kind, intent: CharacterSelectionIntent.self, provider: Provider(), placeholder: PlaceholderView()) { entry in
             EmojiRangerWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("Emoji Ranger Detail")
         .description("Keep track of your favorite emoji ranger.")
-        .supportedFamilies([.systemSmall])
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
@@ -67,11 +113,11 @@ struct EmojiRangerWidget: Widget {
 struct EmojiRangerWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            AvatarView(.panda)
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+            EmojiRangerWidgetEntryView(entry: SimpleEntry(date: Date(), charactor: .panda, relevance: nil))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
             
             PlaceholderView()
-                .previewContext(WidgetPreviewContext(family: .systemSmall))
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
         }
     }
 }
