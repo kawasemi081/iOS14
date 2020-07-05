4a8d8c9305e4e52850a777f274611d91cf918c6c
midori2012 <misono5861@gmail.com>
2020-07-05 22:05:23 +0900

    initial

---
+++ Portfolio/Portfolio/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by MIsono on 2020/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Portfolio/Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by MIsono on 2020/07/05.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
